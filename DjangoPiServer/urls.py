02e5612e7aa9daf844110bd40ab4758c0caf6827
norby7 <hambuernorbi@yahoo.com>
2018-11-11 22:16:30 +0200

    Login templates

--- DjangoPiServer/urls.py
+++ DjangoPiServer/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.urls import views as auth
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view()),
     path('admin/', admin.site.urls),
-    path('',"Hello world")
+    path('reminder/home/', TemplateView.as_view(template_name='reminder/home.html')),
 ]
